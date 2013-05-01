5130720c60ffe2e3ddfadb3ac9f0fe05a7eb2094
Eric J. Holmes <eric@ejholmes.net>
2013-04-30 23:10:57 -0700

    Set redis.

--- deploy.rb
+++ deploy.rb
@@ -1,6 +1,16 @@
 require 'sidekiq'
 require 'open3'
 
+# If your client is single-threaded, we just need a single connection in our Redis connection pool
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1, :url => ENV['REDIS_URL'] }
+end
+
+# Sidekiq server is multi-threaded so our Redis connection pool size defaults to concurrency (-c)
+Sidekiq.configure_server do |config|
+  config.redis = { :url => ENV['REDIS_URL'] }
+end
+
 class Deploy
   include Sidekiq::Worker
 
