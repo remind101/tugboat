d56c1686332d131dfa863a0e95b5340b24535894
Eric J. Holmes <eric@ejholmes.net>
2014-03-21 17:05:06 -0700

    Fix.

--- lib/shipr/notifiers/slack.rb
+++ lib/shipr/notifiers/slack.rb
@@ -1,7 +1,7 @@
 module Shipr
   module Notifiers
     class Slack < Base
-      def notify(payload)
+      def notify
         HTTParty.post "https://#{account}.slack.com/services/hooks/incoming-webhook?token=#{token}",
           body: "payload=#{JSON.dump(text: 'Fuck it')}"
       end
